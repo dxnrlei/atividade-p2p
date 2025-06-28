3296cc12f96b6a50d6e9c75a8154f9d6380b1a84
danrlei <dxnrleilira@gmail.com>
2025-06-28 15:02:14 -0300

    Envio de arquivos públicos

--- client.py
+++ client.py
@@ -18,9 +18,20 @@ class P2PClient:
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
                 sock.connect((self.server_host, self.server_port))
+                
+                # Envia JOIN
                 sock.send("JOIN 127.0.0.1".encode())
                 response = sock.recv(1024).decode()
                 print(f"Resposta do servidor: {response}")
+
+                # Envia arquivos públicos
+                for filename in os.listdir(self.public_folder):
+                    filepath = os.path.join(self.public_folder, filename)
+                    if os.path.isfile(filepath):
+                        size = os.path.getsize(filepath)
+                        sock.send(f"CREATEFILE {filename} {size}".encode())
+                        response = sock.recv(1024).decode()
+                        print(f"Resposta para {filename}: {response}")
         except Exception as e:
             print(f"Erro ao conectar ao servidor: {e}")
 
